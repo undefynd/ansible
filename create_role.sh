f130d0a0f3f16fde50fe2d078d84b354adf7dc5e
undefynd <robertleonschneider@outlook.com>
2020-11-09 14:35:55 +0100

    fixed roles and add functionality
    roles/java*  changed install path from /usr/local/bin to /usr/bin
    roles/jenkins add tasks to install jenkins plugins with authentication
    vagrant/vagrant-hosts.yaml fix double ip entries for vault0* and node0* hosts
    .gitignore add more files to exclude

--- create_role.sh
+++ create_role.sh
@@ -3,6 +3,19 @@
 roles_name=$1
 findroles=$(find . -depth 1 -name roles)
 
-mkdir -p  $findroles/$roles_name/{tasks,handlers,templates,files,vars,defaults,meta}
+if [ -d $findroles/$roles_name ]; then
+  echo "role with the name $roles_name exists"
+  echo "script will be teminated"
+  exit 1
+else
+  echo "role name is unique"
+  echo "creating new role and required subdirectories "
+  mkdir -p  $findroles/$roles_name/{tasks,handlers,templates,files,vars,defaults,meta}
 
-touch $findroles/$roles_name/{tasks,handlers,vars,defaults,meta}/main.yml
+  echo "create default main.yml files"
+  touch $findroles/$roles_name/{tasks,handlers,vars,defaults,meta}/main.yaml
+
+  for i in $(find $findroles/$roles_name -type f ); do
+      echo "---" >> $i
+  done
+fi
